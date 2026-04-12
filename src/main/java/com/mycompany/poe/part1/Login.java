c60109376f80054e49a34b757133a8687a00e2c5
Tshireletso-Pakwe <melokuhlepakwe@gmail.com>
2026-04-12 22:33:28 +0200

    Initial Logic: Added checkUserName and checkPasswordComplexity

--- src/main/java/com/mycompany/poe/part1/Login.java
+++ src/main/java/com/mycompany/poe/part1/Login.java
@@ -1,13 +1,58 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.mycompany.poe.part1;
 
-/**
- *
- * @author Tshireletso
- */
 public class Login {
-    
+   private String username;
+   private String password;
+   private String firstName;
+   private String lastName;
+     public boolean
+    checkUserName(String user) {
+        return user.contains("_")
+         && user.length() <=5;
+    }    
+//    i will be using regular expresssions (Regex) to check the complexity/specifications for the passwords.
+    public boolean
+   checkPasswordComplexity(String pass){
+       String pattern= "^(?=.*[0-9])(?=.*[A-Z])(?=.*[@#$%^&+=!]).{8,}$";
+          return pass.matches(pattern);
+   }
+   
+//   i used the if-statement to make sure that the user's input is correct and meets the conditions/requirements needed. 
+   public String 
+        registerUser(String user,String pass, String fName, String lName)
+        {
+            if(!checkUserName(user))
+            {
+    return "Username is invalid,please ensure that your username contains an underscore and is not longer than 5 characters in length.";
+            }
+            if(!checkPasswordComplexity(pass))
+            {
+    return "Password is invalid,please ensure that the passwor contains at least 8 characters,a capital letter, a number and a special character.";
+            }  
+                this.username= user;
+                this.password= pass;
+                this.firstName= fName;
+                this.lastName= lName;
+    return "Username and password successfully captured.";               
+}
+//   why i created the attempt user & password info was to use it as a security check to check if it matches the original login info(this.username) stored in the memory.
+        public boolean
+    loginUser(String attemptUser, String attemptPass)  
+    {
+        return
+                attemptUser.equals(this.username)
+                    &&
+                attemptPass.equals(this.password);
+    }
+        public String
+                returnLoginStatus(boolean 
+                        isLoggedIn){
+                    if (isLoggedIn){
+                return "Welcome" + firstName+ ","+ lastName+ "it is great to see you again.";
+                    }
+                    else{
+                return"Username or password incorrect,please try again.";
+                    }
+                }
 }

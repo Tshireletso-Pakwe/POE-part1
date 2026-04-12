f043c4f0ccf2dc6fb773b3a5926dd0e418817dd7
Tshireletso-Pakwe <melokuhlepakwe@gmail.com>
2026-04-12 23:43:08 +0200

    Main Class: Implemented user input and login flow

--- src/main/java/com/mycompany/poe/part1/POEPart1.java
+++ src/main/java/com/mycompany/poe/part1/POEPart1.java
@@ -1,16 +1,55 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.poe.part1;
 
-/**
- *
- * @author Tshireletso
- */
+import java.util.Scanner;
 public class POEPart1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+      Scanner sc= new Scanner(System.in);
+      
+      Login loginLogic = new Login();
+      
+    System.out.println("--User Registration--");
+        System.out.print("Enter First Name: ");
+            String firstName =
+                    sc.nextLine();
+        System.out.print("Enter Last Name: ");
+            String lastName =
+                    sc.nextLine();
+        System.out.print("Enter Username:");
+            String username = 
+                    sc.nextLine();
+        System.out.print("Enter Password: ");
+            String password =
+                    sc.nextLine();
+        System.out.print("Enter Cellphone Number: ");
+            String phone =
+                    sc.nextLine();
+        
+   String
+           registrationMessage =
+           loginLogic.registerUser(username, password,firstName, lastName,phone);
+   
+   System.out.println("\nStatus:"+ registrationMessage);
+        if
+                (registrationMessage.equals("User registered successfully"))
+        {
+            System.out.print("Enter Username: ");
+            String loginUser =
+                    sc.nextLine();
+            System.out.print("Enter Password: ");
+            String loginPass =
+                    sc.nextLine();
+            
+    boolean loginSuccess
+            =loginLogic.loginUser(loginUser, loginPass);
+    System.out.println(loginLogic.returnLoginStatus(loginSuccess));
+        }
+        else
+        {
+    System.out.println("Please restart and ensure your formatting is correct.");
+    {
+        sc.close();
+    }
+        }
     }
 }

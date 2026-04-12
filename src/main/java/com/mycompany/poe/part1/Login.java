d6bd4c3cb991a2ea0a5f6f254a1018d02c4e44b1
Tshireletso-Pakwe <melokuhlepakwe@gmail.com>
2026-04-12 23:45:14 +0200

    Input: Added cellphone number

--- src/main/java/com/mycompany/poe/part1/Login.java
+++ src/main/java/com/mycompany/poe/part1/Login.java
@@ -1,4 +1,3 @@
-
 package com.mycompany.poe.part1;
 
 public class Login {
@@ -6,6 +5,7 @@ public class Login {
    private String password;
    private String firstName;
    private String lastName;
+   private String CellPhone;
      public boolean
     checkUserName(String user) {
         return user.contains("_")
@@ -18,9 +18,15 @@ public class Login {
           return pass.matches(pattern);
    }
    
+   public boolean 
+           checkCellPhoneNumber(String number)
+    {
+        return number.matches("^\\=27\\d{9}$");
+    }
+                   
 //   i used the if-statement to make sure that the user's input is correct and meets the conditions/requirements needed. 
    public String 
-        registerUser(String user,String pass, String fName, String lName)
+        registerUser(String user,String pass, String fName, String lName, String phone)
         {
             if(!checkUserName(user))
             {
@@ -30,17 +36,22 @@ public class Login {
             {
     return "Password is invalid,please ensure that the passwor contains at least 8 characters,a capital letter, a number and a special character.";
             }  
-                this.username= user;
-                this.password= pass;
-                this.firstName= fName;
-                this.lastName= lName;
+            if(!checkCellPhoneNumber(phone))
+            {
+    return "Cell phone number is invalid. Please use correct format +27XXXXXXXXX.";
+            }
+                this.username = user;
+                this.password = pass;
+                this.firstName = fName;
+                this.lastName = lName;
+                this.CellPhone = phone;
     return "Username and password successfully captured.";               
 }
 //   why i created the attempt user & password info was to use it as a security check to check if it matches the original login info(this.username) stored in the memory.
         public boolean
     loginUser(String attemptUser, String attemptPass)  
     {
-        return
+        return this.username !=null &&
                 attemptUser.equals(this.username)
                     &&
                 attemptPass.equals(this.password);
